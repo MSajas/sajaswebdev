47bcaa4a8b88a84ee6ec4e8380b8f2455cdcd4ad
SAJAS <121900908+MSajas@users.noreply.github.com>
2024-07-17 05:52:17 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">SAJA </div>
+        <div className="n-name">SAJAS </div>
         <Toggle />
       </div>
       {/* right */}
